b7c92607e08c8eed77249d68355c7e710b6a60e1
Yusuf Kara <94810547+yusufkara95@users.noreply.github.com>
2022-05-24 00:55:27 +0200

    new querys to show the new posts

--- database.php
+++ database.php
@@ -5,4 +5,17 @@ $pdo = new PDO(
     'root',
     'root');
 
+function fetch_posts()
+{
+    global $pdo;
+    return $pdo->query("SELECT * FROM `posts`");
+}
+
+function fetch_post($title)
+{
+    global $pdo;
+    $q = $pdo->query("SELECT * FROM `posts` WHERE title='{$title}'");
+    return $q->fetch();
+}
+
 ?>
