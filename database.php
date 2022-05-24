18e4cae3c9c4404c9e2bc49fd18884e245496d3b
Yusuf Kara <94810547+yusufkara95@users.noreply.github.com>
2022-05-25 00:11:24 +0200

    Change from title to id

--- database.php
+++ database.php
@@ -1,9 +1,18 @@
 <?php
 
 $pdo = new PDO(
-    'mysql:host=localhost;dbname=blog',
+    'mysql:host=localhost;dbname=blog;charset=utf8',
     'root',
     'root');
+
+/*
+$pdo = new PDO(
+    'mysql:host=localhost;dbname=blog;charset=utf8',
+    'root',
+    '');
+*/
+
+$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
 function fetch_posts()
 {
@@ -11,11 +20,20 @@ function fetch_posts()
     return $pdo->query("SELECT * FROM `posts`");
 }
 
-function fetch_post($title)
+function fetch_post($id)
 {
     global $pdo;
-    $q = $pdo->query("SELECT * FROM `posts` WHERE title='{$title}'");
+    $stmt = $pdo->prepare("SELECT * FROM `posts` WHERE id = :id");
+    $stmt->execute(['id' => $id]);
+    return $stmt->fetch();
+
+    /*
+    $query = "SELECT * FROM `posts` WHERE title='{$title}'";
+    var_dump($query);
+    $q = $pdo->query($query);
     return $q->fetch();
+    */
+    
 }
 
 ?>
