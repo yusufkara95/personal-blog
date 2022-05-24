542045127ac6b6b849de5925092dafaa3db1d9e2
Yusuf Kara <94810547+yusufkara95@users.noreply.github.com>
2022-05-25 00:43:59 +0200

    Delete moved code

--- database.php
+++ database.php
@@ -14,26 +14,4 @@ $pdo = new PDO(
 
 $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 
-function fetch_posts()
-{
-    global $pdo;
-    return $pdo->query("SELECT * FROM `posts`");
-}
-
-function fetch_post($id)
-{
-    global $pdo;
-    $stmt = $pdo->prepare("SELECT * FROM `posts` WHERE id = :id");
-    $stmt->execute(['id' => $id]);
-    return $stmt->fetch();
-
-    /*
-    $query = "SELECT * FROM `posts` WHERE title='{$title}'";
-    var_dump($query);
-    $q = $pdo->query($query);
-    return $q->fetch();
-    */
-    
-}
-
 ?>
