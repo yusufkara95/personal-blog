b1b372eaac3dc38be66c6672726e99704096c5b3
Yusuf Kara <94810547+yusufkara95@users.noreply.github.com>
2022-05-30 00:33:54 +0200

    Fix the post model

--- pages/post.php
+++ pages/post.php
@@ -9,10 +9,10 @@
 
 <div class="panel panel-default">
     <div class="panel-heading">
-        <h3 class="panel-title"><?php echo $post->title ?></h3>
+        <h3 class="panel-title"><?php echo $post['title'] ?></h3>
     </div>
     <div class="panel-body">
-        <?php echo nl2br($post->content); ?>
+        <?php echo nl2br($post['content']); ?>
     </div>
 </div>
 
--- src/Post/PostModel.php
+++ src/Post/PostModel.php
@@ -2,12 +2,33 @@
 
 namespace App\Post;
 
-class PostModel
+use ArrayAccess;
+
+class PostModel implements ArrayAccess
 {
     public $id;
     public $title;
     public $content;
 
+    public function offsetExists($offset): bool
+    {
+        return isset($this->$offset);
+    }
+
+    public function offsetGet(mixed $offset): mixed
+    {
+        return $this->$offset;
+    }
+
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        $this->$offset = $value;
+    }
+
+    public function offsetUnset(mixed $offset): void
+    {
+        unset($this->$offset);
+    }
 }
 
 ?>
