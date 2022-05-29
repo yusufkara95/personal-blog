18981243f181745902a1e568a943141033fc286a
Yusuf Kara <94810547+yusufkara95@users.noreply.github.com>
2022-05-30 01:58:33 +0200

    create the receipts for the database connection and posts

--- src/Core/Container.php
+++ src/Core/Container.php
@@ -7,21 +7,68 @@ use App\Post\PostsRepository;
 
 class Container
 {
+
+    private $receipts = [];
+    private $instances = [];
+
+    public function __construct() {
+        $this->receipts = [
+            'postsRepository' => function() {
+                return new PostsRepository(
+                    $this->make("pdo")
+                );
+            },
+            'pdo' => function() {
+                $pdo = new PDO(
+                    'mysql:host=localhost;dbname=blog;charset=utf8',
+                    'root',
+                    'root');
+                $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+                return $pdo;
+            }
+        ];
+    }
+
+    public function make($name)
+    {
+        if (!empty($this->instances[$name]))
+        {
+            return $this->instances[$name];
+        }
+
+        if (isset($this->receipts[$name]))
+        {
+            $this->instances[$name] = $this->receipts[$name]();
+        }
+
+        return $this->instances[$name];
+    }
+    /*
+    private $pdo;
+    private $postsRepository;
+
     public function getPdo()
     {
-        $pdo = new PDO(
+        if (!empty($this->pdo)) {
+            return $this->pdo;
+        }
+        $this->pdo = new PDO(
             'mysql:host=localhost;dbname=blog;charset=utf8',
             'root',
             'root');
 
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        return $pdo;
+        $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        return $this->pdo;
     }
 
     public function getPostsRepository()
     {
+        if (!empty($this->getPostsRepository)) {
+            return $this->getPostsRepository;
+        }
         return new PostsRepository($this->getPdo());
     }
+    */
 }
 
 
