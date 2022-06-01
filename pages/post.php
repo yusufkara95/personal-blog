0b1ea287dd3f3c8e94cb96affc196802c1c670cb
Yusuf Kara <94810547+yusufkara95@users.noreply.github.com>
2022-06-01 15:27:37 +0200

    Delete unnecassary code

--- pages/post.php
+++ pages/post.php
@@ -1,19 +1,6 @@
-<?php include("../init.php"); ?>
-<?php include __DIR__ . "/../layout/header.php"; ?>
+<?php include("../init.php");
 
-<?php
-    $postsRepository = $container->make("postsRepository");
-    $id = $_GET['id'];
-    $post = $postsRepository->fetchPost($id);
+$postsController = $container->make("postsController");
+$postsController->show();
 ?>
 
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title"><?php echo $post['title'] ?></h3>
-    </div>
-    <div class="panel-body">
-        <?php echo nl2br($post['content']); ?>
-    </div>
-</div>
-
-<?php include("elements/footer.php"); ?>
